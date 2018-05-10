c3575ad221de345cddc66adb1b8b9e88a7c333db
maxpermin <maxpermin@outlook.com>
2018-05-09 21:50:18 -0300

    12

---
+++ ejercicio212.vb
@@ -0,0 +1,15 @@
+﻿Module ejercicio212
+    Sub Main()
+        Dim a As UInt16
+        Dim b As UInt16 = 1
+        Dim c As UInt16
+        For f = 0 To 19
+            c = a + b
+            a = b
+            b = c
+            Console.WriteLine(c)
+        Next
+
+    End Sub
+
+End Module
